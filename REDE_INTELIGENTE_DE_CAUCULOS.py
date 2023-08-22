ca6e16df3a767563983f7a557dbd9b02a14af3a2
stoliarSant <95940217+stoliarSant@users.noreply.github.com>
2023-08-22 20:59:37 -0300

    Update REDE_INTELIGENTE_DE_CAUCULOS.py
    ## TODOS OS NUMEROS DA variavel REGRA_DO_JOGO eles serÃO COMBINADOS ATE FORMAR PRIMEIRAMENTE O int(32) EM SEGUIDA COMBINARÁ O int(32)
            ### COM OUTROS NUMEROS DA REGRA DO JOGO E O MESMO COM O int(99)  ELE VAI GERAR MUITOS DADOS DAS COMBINAÇÕES AS QUAIS PODERÃO SER USADAS
            ### PARA DIVERSOS FINS DE COMBINAÇÕES, "O CODIGO PODE SER MODIFICADO E APERFEIÇOADO É GRATUITO PARA O USO DE TODOS.

--- REDE_INTELIGENTE_DE_CAUCULOS.py
+++ REDE_INTELIGENTE_DE_CAUCULOS.py
@@ -7,7 +7,7 @@ import string, random
 
 
 
-
+# ESTE CODIGO COMBINA DIVERSAS FORMULAS
 
 
 
@@ -23,11 +23,13 @@ def movendo_digitos ():
 
             
             
-        campo = ["PARTIDA",int(32),int(66),int(99),"CHEGADA"]
+        campo = ["PARTIDA",int(32),int(66),int(99),"CHEGADA"] ## TODOS OS NUMEROS DA variavel REGRA_DO_JOGO eles serÃO COMBINADOS ATE FORMAR PRIMEIRAMENTE O int(32) EM SEGUIDA COMBINARÁ O int(32)
+        ### COM OUTROS NUMEROS DA REGRA DO JOGO E O MESMO COM O int(99)  ELE VAI GERAR MUITOS DADOS DAS COMBINAÇÕES AS QUAIS PODERÃO SER USADAS
+        ### PARA DIVERSOS FINS DE COMBINAÇÕES, "O CODIGO PODE SER MODIFICADO E APERFEIÇOADO É GRATUITO PARA O USO DE TODOS.
         
         regras_do_jogo = [int(14),int(8),int(9),int(5),int(17),int(16),int(24),int(16),int(33),int(34),int(33),int(334),int(133),int(343),int(733),int(933),int(3333)]
         
-        personagem = float(0.1)
+        personagem = float(0.1)#### FUNÇÃO EM ANDAMENTO
         tiro = random.choice(regras_do_jogo)
         tiro_1 = random.choice(regras_do_jogo)
 
